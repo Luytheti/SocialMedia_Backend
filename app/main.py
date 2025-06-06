f20f893ed254bb2c0dcd05a6024e4dcf6953423e
Luytheti <rajas.darya@gmail.com>
2025-06-06 22:45:23 +0530

    index_page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import user, post, auth, vote
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 #models.Base.metadata.create_all(bind = engine)
 
@@ -21,9 +24,12 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-@app.get("/")
-def root():
-    return {"message" : "Welcome to my api"}
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Welcome to my API"})
 
